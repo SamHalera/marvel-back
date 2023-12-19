49cda3670fea9137d5fa6442e2d4bd0a3d71c2c3
SamHalera <samuel.halera@gmail.com>
2023-12-19 23:00:22 +0100

    testing marvel api

--- routes/testApi.js
+++ routes/testApi.js
@@ -5,27 +5,21 @@ const encBase64 = require("crypto-js/enc-base64");
 
 const router = express.Router();
 
-router.get("/test/comics", async (req, res) => {
+router.get("/test/characters", async (req, res) => {
   try {
-    // const response = await axios.get(
-    //   `http://gateway.marvel.com/v1/public/comics`
-    // );
-    // console.log(response.data);
-    // res.status(200).json(response.data);
-
-    const publicKEY = "08f296a8702b4cb50eb25e61bc0e70b1";
-    const privateKEY = "99964ef5efaba6d1c5064be4e93caafd32368372";
     const ts = Date.now();
 
     const myHash = "b337f111e491803deac55aada4f17fd0";
-    const hash = cripto.MD5(ts + privateKEY + publicKEY).toString(encBase64);
+    const hash = cripto
+      .MD5(ts + process.env.MARVEL_PRIVATE_KEY + process.env.MARVEL_PUBLIC_KEY)
+      .toString(encBase64);
     // const response = await axios.get(
     //   `http://gateway.marvel.com/v1/public/comics?ts=${ts}&apikey=${publicKEY}&hash=${hash}`
     // );
     console.log(
-      `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKEY}&hash=${myHash}`
+      `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${process.env.MARVEL_PUBLIC_KEY}&hash=${myHash}`
     );
-    // res.status(200).json(response.data);
+    res.status(200).json(response.data);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
